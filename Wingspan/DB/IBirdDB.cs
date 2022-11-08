f9dae88b599ed421500ef990157c373b2b3c2ae0
jiajin-zheng <jiajin.zheng@xero.com>
2022-11-08 23:00:01 +1100

    Added BirdsDB

---
+++ Wingspan/DB/IBirdDB.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Wingspan.Model;
+
+namespace Wingspan.DB;
+
+public interface IBirdDB
+{
+    public List<Bird> GetBirds();
+}
---
+++ Wingspan/Model/Bird.cs
@@ -0,0 +1,10 @@
+﻿namespace Wingspan.Model;
+
+public class Bird
+{
+    public string CommonName { get; init; }
+    public string ScientificName { get; init; }
+    public string GameSet { get; init; }
+    public string AbilityColor { get; init; }
+    public string AbilityDescription { get; init; }
+}
