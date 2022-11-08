ad9b93da4164117d7c164f011174154d72ae1137
jiajin-zheng <jiajin.zheng@xero.com>
2022-11-08 22:36:39 +1100

    initial commit

---
+++ Wingspan/GraphQL/Schema/Query.cs
@@ -0,0 +1,12 @@
+﻿
+
+using HotChocolate;
+
+namespace Wingspan.GraphQL.Schema
+{
+    public class Query
+    {
+        [GraphQLDeprecated("This is a test demo query")]
+        public string Instruction => "Hello Jacky!";
+    }
+}
